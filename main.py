e8130a64c9568ebba50a8839d90b5043bc99a3bb
Luis Alberto Garcia Rodriguez <luis_grdz@hotmail.com>
2023-06-26 16:21:49 -0600

    Video 19: Creando ejemplos de Path y Query parameters automaticos: https://platzi.com/clases/2513-fundamentos-fastapi/41910-creando-ejemplos-de-path-y-query-parameters-automa/

--- main.py
+++ main.py
@@ -59,22 +59,22 @@ def create_person(person: Person = Body(...)):
 
 @app.get("/person/detail")
 def show_person(
-        name: Optional[str] = Query(None, min_length=1, max_length=50, title="Person Name", description="It's between 1 and 50 characters"), 
-        age: str = Query(..., title="Person Age", description="Required")
+        name: Optional[str] = Query(None, min_length=1, max_length=50, title="Person Name", description="It's between 1 and 50 characters", example="Luis"), 
+        age: str = Query(..., title="Person Age", description="Required", example=25)
     ):
     return {name: age}
 
 # Validaciones Path parameters
 
 @app.get("/person/detail/{person_id}")
-def show_person(person_id: int = Path(..., gt=0, title="Person Id", description="Person identifier")):
+def show_person(person_id: int = Path(..., gt=0, title="Person Id", description="Person identifier", example=123)):
     return {person_id: "It exists!"}
 
 # Validaciones Request Body 
 
 @app.put("/person/{person_id}")
 def update_person(
-        person_id: int = Path(..., title="Peson Id", description="This is the person Id", gt=0),
+        person_id: int = Path(..., title="Peson Id", description="This is the person Id", gt=0, example=123),
         person: Person = Body(...),
         location: Location = Body(...)
     ):
