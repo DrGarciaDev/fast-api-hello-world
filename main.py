4636b25b1cb6610a1a18ea7d1894ffd8d1f5d6ee
Luis Alberto Garcia Rodriguez <luis_grdz@hotmail.com>
2023-07-07 13:55:16 -0600

    HTTPException, video 10, https://platzi.com/clases/2514-fastapi-errores/41987-httpexception/

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from pydantic import EmailStr
 # FastAPI
 from fastapi import FastAPI
 from fastapi import status
+from fastapi import HTTPException
 from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
 
 app = FastAPI()
@@ -77,9 +78,16 @@ def show_person(
 
 # Validaciones Path parameters
 
+persons = [1,2,3,4,5]
+
 @app.get(path="/person/detail/{person_id}", status_code=status.HTTP_200_OK)
 def show_person(person_id: int = Path(..., gt=0, title="Person Id", description="Person identifier", example=123)):
-    return {person_id: "It exists!"}
+    if person_id not in persons:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="¡ This person doesn't exist !"
+        )
+    return {person_id: "It exists !"}
 
 # Validaciones Request Body 
 
